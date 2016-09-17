3f5b86f08a7f682c4ccedd58f113b3cb1892168b
huergh <estaerk@gmx.de>
2016-09-17 19:00:35 +0200

    hello world
    can open a window with title "Hello World" now

---
+++ src/main/java/calculator/gui/CalculatorApp.java
@@ -0,0 +1,18 @@
+package calculator.gui;
+
+import javafx.application.*;
+import javafx.stage.Stage;
+
+public class CalculatorApp extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Hello World");
+		primaryStage.show();		
+	}
+
+}
