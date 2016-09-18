e2f8d8fe9ed1ff4a9aeaca86db065b6762a4fbc6
huergh <estaerk@gmx.de>
2016-09-18 10:39:53 +0200

    number button events + layout improvements

---
+++ src/main/java/calculator/gui/CalculatorController.java
@@ -0,0 +1,60 @@
+package calculator.gui;
+
+import javafx.fxml.FXML;
+
+public class CalculatorController {
+
+	@FXML
+	protected void buttonZeroPressed() {
+		numberButtonPressed(0);
+	}
+	
+	@FXML
+	protected void buttonOnePressed() {
+		numberButtonPressed(1);
+	}
+	
+	@FXML
+	protected void buttonTwoPressed() {
+		numberButtonPressed(2);
+	}
+	
+	@FXML
+	protected void buttonThreePressed() {
+		numberButtonPressed(3);
+	}
+	
+	@FXML
+	protected void buttonFourPressed() {
+		numberButtonPressed(4);
+	}
+	
+	@FXML
+	protected void buttonFivePressed() {
+		numberButtonPressed(5);
+	}
+	
+	@FXML
+	protected void buttonSixPressed() {
+		numberButtonPressed(6);
+	}
+	
+	@FXML
+	protected void buttonSevenPressed() {
+		numberButtonPressed(7);
+	}
+	
+	@FXML
+	protected void buttonEightPressed() {
+		numberButtonPressed(8);
+	}
+	
+	@FXML
+	protected void buttonNinePressed() {
+		numberButtonPressed(9);
+	}
+
+	private void numberButtonPressed(int number) {
+
+	}
+}
