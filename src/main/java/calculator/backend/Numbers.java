c90e00c5b8b8d0683e9d66fb62c4c4aa8089e0c4
nushei <a@b.c>
2016-09-19 17:09:04 +0200

    corresponding changes to Numbers and Operator classes ( after the two last changes)

--- src/main/java/calculator/backend/Numbers.java
+++ src/main/java/calculator/backend/Numbers.java
@@ -1,5 +1,18 @@
 package calculator.backend;
 
+import java.math.BigDecimal;
+
 public class Numbers implements MathematicalObject{
+	private BigDecimal num;
+
+	public Numbers(BigDecimal num) {
+		this.num = num;
+	}
+
+	public BigDecimal getNum() {
+		return num;
+	}
+	
+	
 
 }
--- src/main/java/calculator/backend/Operator.java
+++ src/main/java/calculator/backend/Operator.java
@@ -1,5 +1,16 @@
 package calculator.backend;
 
-public class Operator implements MathematicalObject{
+public class Operator implements MathematicalObject {
+	private MathOp mathOp;
+
+	public Operator(MathOp mathOp) {
+		this.mathOp = mathOp;
+		}
+
+	public MathOp getMathOp() {
+		return mathOp;
+	}
+	
+	
 
 }
